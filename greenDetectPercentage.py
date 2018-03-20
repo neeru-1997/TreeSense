56567794e20f89dc322302b631ab674bea1715a2
neeru-1997 <34072232+neeru-1997@users.noreply.github.com>
2018-03-20 10:01:10 +0530

    Add files via upload

---
+++ greenDetectPercentage.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+## Read
+img = cv2.imread("TestImage43.png")
+c=0
+## convert to hsv
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+## mask of green (36,0,0) ~ (70, 255,255)
+mask = cv2.inRange(hsv, (40, 0, 0), (70, 255,255))
+for x in mask:
+    for y in x:
+        if(y  > 0):
+            c+=1
+        
+print (c)
+
+percent = float (c) * (100 / (1200*1248))
+print (round(percent))
+
+
+imask = mask>0
+green = np.zeros_like(img, np.uint8)
+green[imask] = img[imask]
+
+## save
+#cv2.imshow("image",green)
+cv2.imwrite("hi43.png", green)
